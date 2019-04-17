ac070033dde70487efc8fe4a3215d0bd58cce3db
Rob Tirserio <rtirserio@gmail.com>
2019-04-17 11:05:15 -0400

    feat: add env var configs

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
+require("dotenv-safe").config();
+
 var express = require("express");
 var bodyParser = require("body-parser");
-var morganBody = require('morgan-body')
+var morganBody = require("morgan-body");
 var routes = require("./routes.js");
 var app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-morganBody(app)
+morganBody(app);
 
 routes(app);
 
-var server = app.listen(3333, function () {
-    console.log("app running on port.", server.address().port);
+var server = app.listen(process.env.PORT, function() {
+  console.log("app running on port.", server.address().port);
 });
