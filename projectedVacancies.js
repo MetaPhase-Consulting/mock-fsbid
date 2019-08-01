ce9daec3813e405ba709bc222e432ab8d40eca9b
Rob Tirserio <rtirserio@gmail.com>
2019-08-01 12:29:30 -0400

    fix: fix filters and support multiple fields with one filter

--- projectedVacancies.js
+++ projectedVacancies.js
@@ -37,8 +37,8 @@ const projectedVacancies = [
     "pos_job_category_desc":  "Management",
     "pos_grade_code": "OC",
     "bureau_desc": "(A)BUREAU OF ADMINISTRATION",
-    "lang1": null,
-    "lang2": null,
+    "lang1": "French(FR) 1/1",
+    "lang2": "Spanish(QB) 3/3",
     "tod": null,
     "bt_differential_rate_num": null,
     "bt_danger_pay_num": null,
@@ -72,6 +72,44 @@ const projectedVacancies = [
     "rnum": 3
   },
 ]
+// TOD filter mappings
+const TODS = [
+  {code: "O", value: "1 YR ( 2 R & R)"},
+  {code: "1", value: "1 YR (3 R & R)"},
+  {code: "D", value: "2 YRS (1 R & R )"},
+  {code: "Q", value: "2 YRS (4 R & R)"},
+  {code: "E", value: "2 YRS/TRANSFER"},
+  {code: "F", value: "2 YRS (2 R & R)"},
+  {code: "R", value: "2 YRS (3R&R)"},
+  {code: "I", value: "3 YRS ( 2 R & R )"},
+  {code: "J", value: "3 YRS/TRANSFER"},
+];
+
+// Custom filter function for TOD
+const todFilter = (filter, field, item) => customFilter(TODS, filter, field, item)
+
+// Language filter mappings
+const LANGUAGES = [
+  {code: "QB", value: "Spanish(QB) 3/3"},
+  {code: "FR", value: "French(FR) 1/1"},
+  {code: "NONE", value: "null"},
+]
+
+// Custom filter function for Languages
+const languageFilter = (filter, field, item) => customFilter(LANGUAGES, filter, field, item)
+/* 
+  Custom filter since we show the value but filter on the code
+  filter - The filter value(s).
+  field - the field on the FILTERS mapping
+  item - The items to check for the presence of the filter
+*/
+const customFilter = (mapping, filter, field, item) => {
+  const filters = mapping.filter(i => filter.includes(i.code)).map(i => i.value)
+  if (item[field] !== undefined && filters.includes(`${item[field]}`)) {
+    return true;
+  }
+  return false;
+}
 
 // Maps filter values to data values
 const FILTERS = {
@@ -82,17 +120,16 @@ const FILTERS = {
   "fv_request_params.pos_numbers": { field: "position" },
   "fv_request_params.skills": { field: "" },
   "fv_request_params.grades": { field: "pos_grade_code" },
-  "fv_request_params.languages": { field: "lang1" },
+  "fv_request_params.languages": { filter: languageFilter, field: ["lang1", "lang2"] },
   "fv_request_params.bureaus": { field: "bureau_code" },
   "fv_request_params.danger_pays": { field: "bt_danger_pay_num" },
   "fv_request_params.bid_seasons": { field: "bsn_id" },
   "fv_request_params.location_codes": { field: "pos_location_code" },
-  "fv_request_params.tod_codes": { field: "tod" }, //?? Need sample data for this field
+  "fv_request_params.tod_codes": { filter: todFilter, field: "tod" }, //?? Need sample data for this field
   "fv_request_params.freeText": { field: "" },
-  "fv_request_params.differential_pays": { field: "differential_rate" },
+  "fv_request_params.differential_pays": { field: "bt_differential_rate_num" },
   "fv_request_params.skills": { field: "skill_code" },
   "fv_request_params.fv_seq_number": { field: "fv_seq_number" },
-  
 }
 
 function get_projected_vacancies(query) {
@@ -100,14 +137,26 @@ function get_projected_vacancies(query) {
   const page_number = query["fv_request_params.page_index"] || 1
   const positions = projectedVacancies.filter(item => {
     for (let key in query) {
-      const field = FILTERS[key] ? FILTERS[key].field : null
+      const fields = FILTERS[key] ? FILTERS[key].field : null
+      let found = false
       // Ignore fields not in filter list (like pagination)
-      if (field) {
-        const filters = Array.isArray(query[key]) ? query[key] : query[key].split(',')
-        console.log(`Search on ${field} with filters ${filters}`)
-        if (item[field] === undefined || !filters.includes(`${item[field]}`)) {
-          return false;
+      if (fields) {
+        const field = Array.isArray(fields) ? fields : [fields]
+        for (let index = 0; index < field.length; index++) {
+          const element = field[index];
+          const filters = Array.isArray(query[key]) ? query[key] : query[key].split(',')
+          console.log(`Search on ${element} with filters ${filters}`)
+          // Check to see if there is a filter function
+          const customFilter = FILTERS[key].filter
+          if (customFilter) {
+            found = found || customFilter(filters, element, item)
+          } else {
+            if (item[element] !== undefined && filters.includes(`${item[element]}`)) {
+              found = found || true;
+            }
+          }
         }
+        return found
       }
     }
     return true;
