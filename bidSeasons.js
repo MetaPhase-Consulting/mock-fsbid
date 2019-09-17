19979a6b30b992cc27763c0b46dc53196b68c039
Rob Tirserio <rtirserio@gmail.com>
2019-09-17 13:31:30 -0400

    fix: get the bidSeason response format correct

--- bidSeasons.js
+++ bidSeasons.js
@@ -42,7 +42,7 @@ const bidSeasons = [
 
 
 function get_bid_seasons(query) {
-  return query.bsn_future_vacancy_ind ? bidSeasons.filter(bidSeason => bidSeason.bsn_future_vacancy_ind === query.future_vacancy_ind) : bidSeasons
+  return { "Data": query.bsn_future_vacancy_ind ? bidSeasons.filter(bidSeason => bidSeason.bsn_future_vacancy_ind === query.future_vacancy_ind) : bidSeasons }
 }
 
 module.exports = { get_bid_seasons }
