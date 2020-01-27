f7a583309172c394c0a0f5974aac2adabb1a5739
Rob Tirserio <rtirserio@gmail.com>
2020-01-27 09:01:21 -0500

    chore: grades

--- seeds/001_positions_seed.js
+++ seeds/001_positions_seed.js
@@ -5,6 +5,7 @@ const locations = readJson('./locations.json')
 const orgs = readJson('./organizations')
 const languages = readJson('./languages.json')
 const bureaus = readJson('./bureaus.json')
+const grades = readJson('./grades.json')
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
@@ -17,6 +18,7 @@ exports.seed = function(knex) {
         position.lang1 = findRandom(languages)['language_code']
         position.lang2 = findRandom(languages)['language_code']
         position.bureau = findRandom(bureaus)['bur']
+        position.pos_grade_code = findRandom(grades)['grade_code']
         return position
 
       })
