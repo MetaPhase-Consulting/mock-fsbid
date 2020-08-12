09d2c0e63ac6a59b4f568eff43be0d2d95e10930
Mike Joyce <mjoyce91@gmail.com>
2020-08-12 11:19:05 -0400

    Update route name

--- src/routes.js
+++ src/routes.js
@@ -64,7 +64,7 @@ var appRouter = function (app) {
     res.end(download)
   });
 
-  app.get("/bids/cycleposition", async function (req, res) {
+  app.get("/cyclepositions/bidders", async function (req, res) {
     let isCDO = false;
     if (!req.headers.jwtauthorization) {
       res.status(200).send({ Data: null, usl_id: 4000004, return_code: -1 })
