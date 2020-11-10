94af5bed2ce0a646ad094378e4eff13658cfb316
elizabeth-jimenez <elizabeth.jimenez.cs@gmail.com>
2020-11-10 16:18:51 -0500

    yaaayy. working!

--- seeds/016_bids_seed.js
+++ seeds/016_bids_seed.js
@@ -1,42 +1,26 @@
-const { findRandom } = require('./data/helpers')
-
 exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex.raw('TRUNCATE TABLE bids CASCADE')
-    .then(function () {
+    // Deletes ALL existing entries
+    return knex.raw('TRUNCATE TABLE bids CASCADE')
+        .then(function () {
 
-      return knex.select('cp_id').from('availablepositions').then(ap => {
-          const bids = []
-
-          const bureauPositions = knex
+            return knex.select('cp_id').from('availablepositions')
               .innerJoin('positions', 'positions.position', 'availablepositions.position')
-              .whereIn('bureau', ['120000', '170000', '160000', '260000']);
-
-          const allEmp =  knex
-            .from('employees')
-            .innerJoin('employees_roles', 'employees.perdet_seq_num', 'employees_roles.perdet_seq_num');
-          allEmp.where('employees_roles.code', 'fsofficer')
-            .select('employees.perdet_seq_num').then(e => {
-                e.forEach(emp => {
-                  for (let index = 0; index < 5; index++) {
-                    bids.push({
-                      perdet_seq_num: emp.perdet_seq_num,
-                      cp_id: findRandom(ap)['cp_id'],
-                    })
-                  }
-                })
-              });
-          bureauPositions.forEach(cpid => {
-              allEmp.select('employees.perdet_seq_num').then(emp => {
-                  emp.forEach(peremp => {
+              .whereIn('bureau', ['120000', '170000'])
+              .then(cpids => {
+                return knex
+                    .from('employees')
+                    .select('employees.perdet_seq_num').then(e => {
+                        const bids = []
+                        e.forEach(emp => {
+                        cpids.forEach(cpid => {
                           bids.push({
-                              perdet_seq_num: peremp.perdet_seq_num,
-                              cp_id: cpid,
+                            perdet_seq_num: emp.perdet_seq_num,
+                            cp_id: cpid['cp_id'],
                           })
-                  })
-              });
-          });
-            return knex('bids').insert(bids);
-          })
-    });
+                        })
+                      })
+                     return knex('bids').insert(bids);
+                })
+            })
+        });
 };
