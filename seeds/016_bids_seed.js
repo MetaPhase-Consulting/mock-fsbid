c06eab73adcb591058092efc286a0fc2fcb53b4d
elizabeth-jimenez <elizabeth.jimenez.cs@gmail.com>
2020-11-10 16:26:56 -0500

    updating spacing

--- seeds/016_bids_seed.js
+++ seeds/016_bids_seed.js
@@ -1,26 +1,26 @@
 exports.seed = function(knex) {
-    // Deletes ALL existing entries
-    return knex.raw('TRUNCATE TABLE bids CASCADE')
-        .then(function () {
+// Deletes ALL existing entries
+  return knex.raw('TRUNCATE TABLE bids CASCADE')
+    .then(function () {
 
-            return knex.select('cp_id').from('availablepositions')
-              .innerJoin('positions', 'positions.position', 'availablepositions.position')
-              .whereIn('bureau', ['120000', '170000'])
-              .then(cpids => {
-                return knex
-                    .from('employees')
-                    .select('employees.perdet_seq_num').then(e => {
-                        const bids = []
-                        e.forEach(emp => {
-                        cpids.forEach(cpid => {
-                          bids.push({
-                            perdet_seq_num: emp.perdet_seq_num,
-                            cp_id: cpid['cp_id'],
-                          })
-                        })
-                      })
-                     return knex('bids').insert(bids);
+      return knex.select('cp_id').from('availablepositions')
+        .innerJoin('positions', 'positions.position', 'availablepositions.position')
+        .whereIn('bureau', ['120000', '170000'])
+        .then(cpids => {
+          return knex
+            .from('employees')
+            .select('employees.perdet_seq_num').then(e => {
+              const bids = []
+              e.forEach(emp => {
+                cpids.forEach(cpid => {
+                  bids.push({
+                    perdet_seq_num: emp.perdet_seq_num,
+                    cp_id: cpid['cp_id'],
+                  })
                 })
+              })
+              return knex('bids').insert(bids);
             })
-        });
+        })
+    });
 };
