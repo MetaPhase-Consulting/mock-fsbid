0800d26fb1502561b7c6eaf0500a08128bee498a
scott062 <scott.m.burdick@gmail.com>
2021-02-02 19:22:14 -0500

    Newest migration file - WIP - please do not run this yet until we confirm backouts

--- migrations/054_classifications_updates.js
+++ migrations/054_classifications_updates.js
@@ -1,14 +1,38 @@
 exports.up = function(knex) {
   return knex.schema
+    .alterTable('classifications', function(table) {
+      table.integer('te_id')
+      table.string('te_descr_txt')
+      table.integer('rnum')
+      table.unique('te_id')
+    })
     .alterTable('employees_classifications', function(table) {
-      table.integer('bsn_id').nullable()
-      table.unique(['td_id', 'perdet_seq_num'])
-    });
+      table.dropForeign('td_id')
+      table.dropColumn('td_id')
+    })
+    .alterTable('classifications', function(table) {
+      table.dropColumn('td_id')
+    })
+    .alterTable('employees_classifications', function(table) {
+      table.increments('td_id').primary()
+      table.integer('te_id')
+      table.foreign('te_id').references('classifications.te_id')
+      table.unique(['te_id', 'perdet_seq_num'])
+    })
+    
 };
 exports.down = function(knex) {
   return knex.schema
+    .alterTable('classifications', function(table) {
+      table.string('td_id')
+      table.dropColumns('te_id', 'te_descr_txt', 'rnum')
+    })
     .alterTable('employees_classifications', function(table) {
-      table.dropUnique(['td_id', 'perdet_seq_num'])
-      table.dropColumn('bsn_id')
-    });
+      table.dropColumn('td_id')
+      table.foreign('td_id').references('classifications.td_id')
+
+      table.dropForeign('te_id')
+      table.dropColumn('te_id')
+      table.dropUnique(['te_id', 'perdet_seq_num'])
+    })
 };
