fdac6d4c9a5fc456bc680d06450ef60529a53122
scott062 <scott.m.burdick@gmail.com>
2021-02-03 10:09:26 -0500

    Fix rollbacks

--- migrations/054_classifications_updates.js
+++ migrations/054_classifications_updates.js
@@ -23,16 +23,21 @@ exports.up = function(knex) {
 };
 exports.down = function(knex) {
   return knex.schema
-    .alterTable('classifications', function(table) {
-      table.string('td_id')
-      table.dropColumns('te_id', 'te_descr_txt', 'rnum')
-    })
     .alterTable('employees_classifications', function(table) {
       table.dropColumn('td_id')
-      table.foreign('td_id').references('classifications.td_id')
-
+      table.dropUnique(['te_id', 'perdet_seq_num'])
       table.dropForeign('te_id')
       table.dropColumn('te_id')
-      table.dropUnique(['te_id', 'perdet_seq_num'])
+    })
+    .alterTable('classifications', function(table) {
+      table.increments('td_id').primary()
+    })
+    .alterTable('employees_classifications', function(table) {
+      table.integer('td_id')
+      table.foreign('td_id').references('classifications.td_id')
+    })
+    .alterTable('classifications', function(table) {
+      table.dropUnique('te_id')
+      table.dropColumns('te_id', 'te_descr_txt', 'rnum')
     })
 };
