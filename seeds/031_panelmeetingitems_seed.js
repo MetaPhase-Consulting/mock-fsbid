e875e428eec03302a51741bdcc35b54f51854069
elizabeth-jimenez <elizabeth.jimenez.cs@gmail.com>
2022-01-13 15:46:18 -0500

    pmi seed completed

--- seeds/031_panelmeetingitems_seed.js
+++ seeds/031_panelmeetingitems_seed.js
@@ -9,47 +9,46 @@ exports.seed = function(knex) {
         .then(PMs => {
           return knex.select('miccode').from('panelmeetingitemcategories')
             .then(MICs => {
-              const panel_meeting_items = [];
-              let pmiseqnum = 0;
-              const aoCdoPerdets = [2, 7, 8, 13];
-              PMs.forEach(pm => {
-                  let pmseqnum = pm.pmseqnum;
-                  let mic = _.sample(MICs);
-                  // 15-20 pmi per pmseqnum; 60 for pmiseqnum=60
-                  let numOfPMIs = randomIntInclusive(15, 20);
-                  numOfPMIs = pmseqnum === 60 ? 60 : numOfPMIs;
-                  for(let k = 1; k <= numOfPMIs; k++) {
-                    pmiseqnum+=1;
-                    // grab the pmd_dttm using pmseqnum from panel meeting date table
-                    // 2 weeks to 5 days before pmd_dttm
-                    let createDate = datefns.subDays(pmd_dttm, randomIntInclusive(5, 14));
-                    // createDate to 3 days before pmd_dttm
-                    let rangeleft = datefns.addMinutes(createDate, 3);
-                    let rangeRight = datefns.subDays(pmd_dttm, 3);
-                    let range = datefns.differenceInDays(rangeleft, rangeRight);
-                    let updateDate = datefns.subDays(createDate, Math.floor(Math.random() * range));
-                    panel_meeting_items.push({
-                      pmiseqnum: pmiseqnum,
+              return knex.select('mdtcode').from('panelmeetingdates')
+                .then(MDTs => {
+                  const panel_meeting_items = [];
+                  let pmiseqnum = 0;
+                  const aoCdoPerdets = [2, 7, 8, 13];
+                  PMs.forEach(pm => {
+                      let pmseqnum = pm.pmseqnum;
+                      let pmddttm = _.find(MDTs, { 'pmseqnum': pmseqnum })['pmddttm'] || null;
+                      let mic = _.sample(MICs);
+                      // 15-20 pmi per pmseqnum; 60 for pmiseqnum=60
+                      let numOfPMIs = randomIntInclusive(15, 20);
+                      numOfPMIs = pmseqnum === 60 ? 60 : numOfPMIs;
+                      for(let k = 1; k <= numOfPMIs; k++) {
+                        pmiseqnum+=1;
+                        let createDate = datefns.subDays(pmddttm, randomIntInclusive(5, 14));
+                        let rangeleft = datefns.addMinutes(createDate, 3);
+                        let rangeRight = datefns.subDays(pmddttm, 3);
+                        let range = datefns.differenceInDays(rangeleft, rangeRight);
+                        let updateDate = datefns.subDays(createDate, Math.floor(Math.random() * range));
+                        panel_meeting_items.push({
+                          pmiseqnum: pmiseqnum,
 
-                      pmseqnum: pmseqnum,
-                      miccode: mic.miccode,
+                          pmseqnum: pmseqnum,
+                          miccode: mic.miccode,
 
-                      // important number that gets called out at pre+panel. Unique on the pmseqnum
-                      pmiofficialitemnum: k,
-                      pmiaddendumind: 'N',
-                      pmilabeltext: _.sample([' ', 'AF', 'A']),
-                      pmicreateid: _.sample(aoCdoPerdets),
-                      // TODO: Until when can create and update Panel Meeting Items and Agendas be edited once attached to a Panel Meeting
-
-                      // TODO: last two to complete in the file
-                      // pmicreatedate: createDate,
-                      pmiupdateid: _.sample(aoCdoPerdets),
-                      // pmiupdatedate: updateDate,
+                          // important number that gets called out at pre+panel. Unique on the pmseqnum
+                          pmiofficialitemnum: k,
+                          pmiaddendumind: 'N',
+                          pmilabeltext: _.sample([' ', 'AF', 'A']),
+                          pmicreateid: _.sample(aoCdoPerdets),
+                          // TODO: Until when can create and update Panel Meeting Items and Agendas be edited once attached to a Panel Meeting
+                          pmicreatedate: createDate,
+                          pmiupdateid: _.sample(aoCdoPerdets),
+                          pmiupdatedate: updateDate,
+                        });
+                      }
                     });
-                  }
-                });
 
-              return knex.batchInsert('panelmeetingitems', panel_meeting_items, 500);
+                  return knex.batchInsert('panelmeetingitems', panel_meeting_items, 500);
+              });
             });
         });
     });
