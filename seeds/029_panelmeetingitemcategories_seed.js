f4218ecf81ff6489922e57556ab9346c6daea540
elizabeth-jimenez <elizabeth.jimenez.cs@gmail.com>
2022-01-13 08:46:16 -0500

    seed for mic completedw

--- seeds/029_panelmeetingitemcategories_seed.js
+++ seeds/029_panelmeetingitemcategories_seed.js
@@ -1,4 +1,6 @@
 const { readJson } = require('./data/helpers')
+const datefns = require('date-fns')
+const { randomIntInclusive } = require('./data/helpers')
 
 const panelmeetingitemcategories = readJson('./panelmeetingitemcategories.json')
 
@@ -6,14 +8,17 @@ exports.seed = function(knex) {
   // Deletes ALL existing entries
   return knex.raw('TRUNCATE TABLE panelmeetingitemcategories CASCADE')
     .then(function () {
-      // TODO: seed the following
-      // "miccreateid": 2,
-      //    "miccreatedate": "2004-05-05T00:00:00",
-      //     "micupdateid": 2,
-      //     "micupdatedate": "2004-05-05T00:00:00"
+      const panel_meeting_item_categories = [];
+      const createDate = datefns.subDays(Date.now(), randomIntInclusive(250, 365));
+      panelmeetingitemcategories.forEach(mic => {
+        panel_meeting_item_categories.push({
+          miccreateid: 2,
+          miccreatedate: createDate,
+          micupdateid: 2,
+          micupdatedate: datefns.addDays(createDate, randomIntInclusive(10, 60)),
+        });
+      });
 
-
-      // Inserts seed entries
       return knex('panelmeetingitemcategories').insert(panelmeetingitemcategories);
     });
 };
