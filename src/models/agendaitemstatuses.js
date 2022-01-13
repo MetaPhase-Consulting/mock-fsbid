cbf149142ccf99e98f9dd0d9ee8bf7bd6edfb3b7
elizabeth-jimenez <elizabeth.jimenez.cs@gmail.com>
2022-01-13 13:21:47 -0500

    removing aistatus files, done in another PR; updating migration numbers

--- src/models/agendaitemstatuses.js
+++
@@ -1,8 +0,0 @@
-const bookshelf = require('../bookshelf.js')
-
-const AgendaItemStatuses = bookshelf.model('AgendaItemStatuses', {
-  tableName: 'agendaitemstatuses',
-  idAttribute: 'aiscode'
-})
-
-module.exports = AgendaItemStatuses
