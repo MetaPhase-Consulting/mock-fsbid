5c9b10a60c572483d262e2a76dc8f390e2020cc4
Mike Joyce <joyce_michael@bah.com>
2022-01-27 16:14:29 -0500

    Seed A status panels; update migration number

--- seeds/027_panelmeetings_seed.js
+++ seeds/027_panelmeetings_seed.js
@@ -18,7 +18,7 @@ exports.seed = function(knex) {
                 } else if (i > arr.length / 2) {
                   pmscode$ = 'P';
                 } else if (i > arr.length / 4) {
-                  pmscode$ = 'I';
+                  pmscode$ = _.sample(['I', 'A']);
                 } 
                 panel_meetings.push({
                   pmseqnum: i + 1,
