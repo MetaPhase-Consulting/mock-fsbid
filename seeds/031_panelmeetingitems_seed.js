10cfbb36deadd48b06f9b4748496fa4aa23d1471
elizabeth-jimenez <elizabeth.jimenez.cs@gmail.com>
2022-02-01 13:58:44 -0500

    prettier

--- seeds/031_panelmeetingitems_seed.js
+++ seeds/031_panelmeetingitems_seed.js
@@ -15,39 +15,38 @@ exports.seed = function(knex) {
                   let pmiseqnum = 0;
                   const aoCdoPerdets = [2, 7, 8, 13];
                   PMs.forEach(pm => {
-                      let pmseqnum = pm.pmseqnum;
-                      const pmddttm = _.find(MDTs, { 'pmseqnum': pmseqnum, 'mdtcode': 'MEET' })
-                    const pmddttm$ = pmddttm['pmddttm'];
-                      let mic = _.sample(MICs);
-                      // 15-20 pmi per pmseqnum; 60 for pmiseqnum=60
-                      let numOfPMIs = randomIntInclusive(15, 20);
-                      numOfPMIs = pmseqnum === 60 ? 60 : numOfPMIs;
-                      for(let k = 1; k <= numOfPMIs; k++) {
-                        pmiseqnum+=1;
-                        let createDate = datefns.subDays(pmddttm, randomIntInclusive(5, 14));
-                        let rangeleft = datefns.addMinutes(createDate, 3);
-                        let rangeRight = datefns.subDays(pmddttm, 3);
-                        let range = datefns.differenceInDays(rangeleft, rangeRight);
-                        let updateDate = datefns.subDays(createDate, Math.floor(Math.random() * range));
-                        panel_meeting_items.push({
-                          pmiseqnum: pmiseqnum,
-
-                          pmseqnum: pmseqnum,
-                          miccode: mic.miccode,
-
-                          // important number that gets called out at pre+panel. Unique on the pmseqnum
-                          pmiofficialitemnum: k,
-                          pmiaddendumind: 'N',
-                          pmilabeltext: _.sample([' ', 'AF', 'A']),
-                          pmicreateid: _.sample(aoCdoPerdets),
-                          // TODO: Until when can create and update Panel Meeting Items and Agendas be edited once attached to a Panel Meeting
-                          pmicreatedate: createDate,
-                          pmiupdateid: _.sample(aoCdoPerdets),
-                          pmiupdatedate: updateDate,
-                        });
-                      }
-                    });
-
+                    let { pmseqnum } = pm;
+                    const numPMIsLookup = {
+                      60: 60,
+                      200: 200,
+                      300: 300,
+                      'default': randomIntInclusive(15, 20),
+                    };
+                    const pmddttm = _.find(MDTs, { 'pmseqnum': pmseqnum, 'mdtcode': 'MEET' })['pmddttm']
+                    let mic = _.sample(MICs);
+                    // 15-20 pmi per pmseqnum; 60 for pmiseqnum=60
+                    const numOfPMIs = _.get(numPMIsLookup, pmseqnum) || numPMIsLookup['default'];
+                    for(let k = 1; k <= numOfPMIs; k++) {
+                      pmiseqnum+=1;
+                      let createDate = datefns.subDays(pmddttm, randomIntInclusive(5, 14));
+                      let rangeleft = datefns.addMinutes(createDate, 3);
+                      let rangeRight = datefns.subDays(pmddttm, 3);
+                      let range = datefns.differenceInDays(rangeleft, rangeRight);
+                      let updateDate = datefns.subDays(createDate, Math.floor(Math.random() * range));
+                      panel_meeting_items.push({
+                        pmiseqnum: pmiseqnum,
+                        pmseqnum: pmseqnum,
+                        miccode: mic.miccode,
+                        pmiofficialitemnum: k,
+                        pmiaddendumind: 'N',
+                        pmilabeltext: _.sample([' ', 'AF', 'A']),
+                        pmicreateid: _.sample(aoCdoPerdets),
+                        pmicreatedate: createDate,
+                        pmiupdateid: _.sample(aoCdoPerdets),
+                        pmiupdatedate: updateDate,
+                      });
+                    }
+                  });
                   return knex.batchInsert('panelmeetingitems', panel_meeting_items, 500);
               });
             });
