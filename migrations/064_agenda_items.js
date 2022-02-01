cf503da1fc69f2601bed75a40091decc9935abac
elizabeth-jimenez <elizabeth.jimenez.cs@gmail.com>
2022-02-01 15:21:39 -0500

    Update 064_agenda_items.js

--- migrations/064_agenda_items.js
+++ migrations/064_agenda_items.js
@@ -2,7 +2,6 @@ exports.up = function(knex) {
   return knex.schema
     .createTable('agendaitems', function(table) {
         table.increments('aiseqnum').primary()
-        // table.integer('aiseqnum')
 
         table.integer('pmiseqnum')
         table.string('aiscode')
