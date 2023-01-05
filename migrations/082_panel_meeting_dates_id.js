b1c75573784743edd21f397625d04995b3fe24e2
elizabeth <elizabeth.jimenez.cs@gmail.com>
2023-01-04 21:04:24 -0500

    modifying migration for panel meeting dates

--- migrations/082_panel_meeting_dates_id.js
+++ migrations/082_panel_meeting_dates_id.js
@@ -1,25 +1,16 @@
-exports.up = function (knex) {
+exports.up = function(knex) {
   return knex.schema
-    .alterTable('frequentpositions', function (table) {
-      table.dropColumn('posseqnum')
-      table.dropColumn('posorgshortdesc')
-      table.dropColumn('posnumtext')
-      table.dropColumn('posgradecode')
-      table.dropColumn('postitledesc')
-    })
-    .alterTable('frequentpositions', function (table) {
-      table.integer('posseqnum').primary();
-      table.foreign('posseqnum').references('pos_seq_num').inTable('positions')
+    .alterTable('panelmeetingdates', function(table) {
+      table.dropColumn('id')
+
+      table.increments('testing_this_id').primary();
     });
 };
-
-exports.down = function (knex) {
+exports.down = function(knex) {
   return knex.schema
-    .alterTable('frequentpositions', function (table) {
-      table.string('posorgshortdesc')
-      table.string('posnumtext')
-      table.string('posgradecode')
-      table.string('postitledesc')
-      table.dropForeign('posseqnum')
+    .alterTable('panelmeetingdates', function(table) {
+      table.string('id')
+
+      table.dropColumn('testing_this_id')
     });
 };
