bddeb58ac204410fe239d79d0686740563dfabd3
elizabeth <elizabeth.jimenez.cs@gmail.com>
2024-02-14 10:22:15 -0500

    why

---
+++ seeds/data/helpers.js
@@ -0,0 +1,11 @@
+const fs = require('fs')
+
+const readJson = path => JSON.parse(fs.readFileSync(require.resolve(path)))
+
+const findRandom = array => array[Math.floor(Math.random() * array.length)]
+
+const randomIntInclusive = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+module.exports = { readJson, findRandom, randomIntInclusive }
